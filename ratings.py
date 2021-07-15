04647be5c4f0edbda8342b48bc383edccb1e7fda
Avery.Davenport <averymdavenport@gmail.com>
2021-07-15 12:22:37 -0600

    polished up the system

--- ratings.py
+++ ratings.py
@@ -1,16 +1,18 @@
 """Restaurant rating lister."""
 
 
+
 def get_scores():
-    score = open('scores.txt')
     scores = {}
+    score = open('scores.txt')
     for i in score:
         restaurant, score = i.split(':')
         scores[restaurant] = int(score)
     
-    new_scores = add_restaurants(scores)
+    new_scores = add_restaurants()
+    scores.update(new_scores)
 
-    return display_restaurants(sorted(new_scores.items()))
+    return display_restaurants(sorted(scores.items()))
 
 
 def display_restaurants(scores):
@@ -18,15 +20,21 @@ def display_restaurants(scores):
         print(i,"has a score of", str(x)+"!")
 
 
-def add_restaurants(scores):
-    new_scores = scores
-    new = input("Would you like to add a new review? Yes or No:\n")
-
-    if new == "Yes":
-        new_name = input("What is the restaurants name?\n")
+def add_restaurants():
+    new_scores = []
+    new = input("Would you like to add a new review? Yes or No:\n").lower()
+    
+    if new == "yes":
+        new_scores = {}
+        new_name = input("What is the restaurants name?\n").capitalize()
         new_rating = input("What would you rate this resaurant between 1-5?\n")
         new_scores[new_name] = new_rating
-    
+    elif new == "no":
+        return new_scores
+    else:
+        print("Sorry I didn't quite understand that")
+        return add_restaurants()
+        
     return new_scores
 
 
