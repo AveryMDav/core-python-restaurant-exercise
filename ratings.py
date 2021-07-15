a14931f638526d5258d30542731433ec3ba9aa08
Avery.Davenport <averymdavenport@gmail.com>
2021-07-15 16:23:47 -0600

    further study 1 added, working on further study 2

--- ratings.py
+++ ratings.py
@@ -1,24 +1,44 @@
 """Restaurant rating lister."""
 
 
-
-def get_scores():
+def start_up():
     scores = {}
     score = open('scores.txt')
+    print("Select an option\n"
+        "See all ratings: 1\n"
+        "Add a new restaurant: 2\n"
+        "Quit program: 3\n")
+    options = int(input("Input option number to select\n"))
+
+    if options == 1:
+        get_scores(score, scores)
+
+    elif options == 2:
+            new_scores = add_restaurants()
+            scores.update(new_scores)
+            return start_up()
+
+    elif options == 3:
+        print('3')
+
+    else:
+        print("This is not a valid option")
+        return start_up()
+
+
+
+def get_scores(score, scores):
     for i in score:
         restaurant, score = i.split(':')
         scores[restaurant] = int(score)
-    
-    new_scores = add_restaurants()
-    scores.update(new_scores)
 
     return display_restaurants(sorted(scores.items()))
 
 
 def display_restaurants(scores):
     for i, x in scores:
-        print(i,"has a score of", str(x)+"!")
-
+        print(i,"has a score of", str(x)+".")
+    return start_up()
 
 def add_restaurants():
     new_scores = {}
@@ -26,15 +46,23 @@ def add_restaurants():
     
     if new == "yes":
         new_name = input("What is the restaurants name?\n").capitalize()
-        new_rating = input("What would you rate this resaurant between 1-5?\n")
+        new_rating = rating()
+        
         new_scores[new_name] = new_rating
     elif new == "no":
-        return new_scores
+        pass
     else:
         print("Sorry I didn't quite understand that")
         return add_restaurants()
         
     return new_scores
 
-
-get_scores()
+def rating():
+    new_rating = input("What would you rate this resaurant between 1-5?\n")
+    if int(new_rating) <= 5 and int(new_rating) >= 1:
+        return new_rating
+    else:
+        print("Please input a rating of 1-5")
+        return rating()
+        
+start_up()
